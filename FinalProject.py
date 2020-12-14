0327c659792e58ac19d31bd28aede143f6a7fc0f
EdgZepeda93 <75556573+EdgZepeda93@users.noreply.github.com>
2020-12-13 23:56:35 -0500

    Add files via upload

--- FinalProject.py
+++ FinalProject.py
@@ -2,20 +2,11 @@ import os
 
 f1 =  open("GameRating.txt", "a")
 
-def r(rating):
-    if rating in range(1,6):
-        print()
-    else:
-        print()
-        exit()
-def s(gameRating):
-    for line in f1:
-        line = line.split('::')
-        ratedGameList = line[0]
-        rate = line[1]
-        gameRate = (ratedGameList + '::' + rate)
-        print(gameRate)
-    f1.close()
+def checkIfWithinRange(rating):
+  if rating not in range(1,6):
+    print("\nRating must be within 1-5! Try again.\n")
+    return 0
+  return 1
 
 # Populates the games dictionary with games and rating from the file.
 def populateGamesDict(gamesDict):
@@ -27,57 +18,87 @@ def populateGamesDict(gamesDict):
       gamesDict[game] = rating
   f1.close()
 
-
-games = {}
-if(os.stat("GameRating.txt").st_size > 0):
-  populateGamesDict(games)
-
-print(games);
-option = input("Enter 1 to add game, Enter 2 to sort by rating, Enter 3 for hours played: ")
-if option == "1":
-    while True:
-        f1 = open("GameRating.txt", "a")
-        game = input("Please Enter Game title or 'E' to exit: ").capitalize()
-        if(game == "E"):
-            break;
-        rating = int(input("Enter rating from 1-5: "))
-        hours = int(input('Enter number of hours played: '))
-        gameRating = (game + '::' + str(rating) + '::' + str(hours))
-        r(rating)
-        games[game] = rating
-        f1.write(gameRating + '\n')
-        f1.close()
-    print(games)
-    f1.close()
-
-elif option == '2':
-    f1 = open("GameRating.txt", 'r')
-    gameList = []
-    userRating = int(input("Enter a rating you want to output games for:"))
-    for key, value in games.items():
-      if(int(value) == int(userRating)):
-        gameList.append(key)
-    gameList.sort()
-    for game in gameList:
-      print(game)
-
-elif option == '3':
-    f1 = open("GameRating.txt", 'r')
-    hours = dict()
+def populateHoursDict(gameHours):
+    f1 = open("GameRating.txt", "r")
     for line in f1:
         line = line.split('::')
-        hours = line[1]
-        print(hours)
+        game = line[0]
+        hours = line[2]
+        gameHours[game] = hours.rstrip("\n")
     f1.close()
-else:
+
+def printSortedListItems(list):
+  list.sort()
+  for item in list:
+    print(" • " + item)
+
+games = {}
+gameHours = {}
+option = ""
+
+# Source: https://www.tutorialspoint.com/python/os_stat.htm
+# Checking to see if 'GameRating.txt' is empty
+if(os.stat("GameRating.txt").st_size > 0):
+  populateGamesDict(games)
+  populateHoursDict(gameHours)
+
+print("----------------------------")
+print("Game Rating and Hour Tracker")
+print("----------------------------")
+
+while(option != "e"):
+
+  print("\nMenu:")
+  print("a) Add Game\nb) Sort Games By Rating\nc) Sort Games By Specified Hours\ne) Exit program\n")
+
+  option = input("Enter an option: ")
+  print("\n")
+
+  if option == "a":
+      while True:
+          f1 = open("GameRating.txt", "a")
+          game = input("Enter Game title or 'x' to exit: ")
+
+          if(game == "x"):
+            print("\n")
+            break;
+
+          isInRange = 0
+          while(isInRange == 0):
+            rating = int(input("Enter a rating from 1-5: "))
+            isInRange = checkIfWithinRange(rating)
+
+          hours = int(input('Enter number of hours played: '))
+          gameRating = (game + '::' + str(rating) + '::' + str(hours))
+          games[game] = rating
+          f1.write(gameRating + "\n")
+          f1.close()
+      f1.close()
+
+  elif option ==  "b":
+      f1 = open("GameRating.txt", 'r')
+      gameList = []
+      userRating = int(input("Enter a rating between 1-5: "))
+
+      for key, value in games.items():
+        if(int(value) == int(userRating)):
+          gameList.append(key)
+
+      printSortedListItems(gameList)
+
+  elif option == "c":
+      f1 = open("GameRating.txt", 'r')
+      hoursList = []
+      hours = int(input("Enter hours: "))
+
+      for key, value in gameHours.items():
+        if(int(value) <= int(hours)):
+          hoursList.append(key)
+
+      printSortedListItems(hoursList)
+
+  elif option == "e":
+    print("\nExiting ...\n")
     exit()
-#add hours
-#display games in alphabetical order sorting
-# while True:
-# display menu options
-# ask user for option
-# if input equals a -> ask to enter game and rating
-# if input equals b -> ask user for a rating and display games for that rating
-# if input equals e -> exit program
-# create a menu by using an if statement
-# take in user input
+  else:
+    print("\nInvalid option! Try again.\n")
